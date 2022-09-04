b82811cc742b3a25ec327f462287c2aa54f41698
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-04 17:09:47 -0400

    Create odoo_run.sh

---
+++ tool/odoo_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir models
+touch models/__init__.py
+mkdir controllers
+touch controllers/__init__.py
+mkdir views
+touch views/views.xml
+mkdir security
+mkdir wizard
+touch wizard/__init__.py
+mkdir report
+mkdir data
+mkdir demo
+mkdir i18n
