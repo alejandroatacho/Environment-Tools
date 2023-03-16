6f1080ba75b07c216eb9d1a0d532c02f087566c3
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-16 10:17:03 -0400

    Update odoo_v14.sh

--- tools/odoo/odoo_v14.sh
+++ tools/odoo/odoo_v14.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
+
 # make odoo v14 folders to start
-cd .. 
 cd ..
+cd ..
+if [ ! -d "odoo_module_rename_me" ]; then
 mkdir odoo_module_rename_me
 cd odoo_module_rename_me
 mkdir models
-touch models/__init__.py
+touch models/init.py
 mkdir controllers
-touch controllers/__init__.py
+touch controllers/init.py
 mkdir views
 touch views/views.xml
 mkdir security
 mkdir wizard
-touch wizard/__init__.py
+touch wizard/init.py
 mkdir report
 mkdir data
 mkdir demo
 mkdir i18n
-
+touch __manifest__.py
+echo "{
+    'name': 'Module Name',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'description': 'Module description',
+    'author': 'Hinamizawa',
+    'website': 'https://github.com/alejandroatacho',
+    'depends': ['base'],
+    'data': [
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}" > __manifest__.py
+else
+echo "odoo_module_rename_me already exists, skipping folder creation."
+fi
