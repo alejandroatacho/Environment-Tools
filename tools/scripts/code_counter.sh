b97f706ce380a12b650ccf0027efadaac0dfbd49
Hinamizawa <hinamizawa.github@gmail.com>
2023-04-01 18:18:32 -0400

    update

--- tools/scripts/code_counter.sh
+++ tools/scripts/code_counter.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
 
 cd ..
-total_scss_lines=0
-total_css_lines=0
-total_js_lines=0
-total_html_lines=0
-total_php_lines=0
 
-for file in $(find . -type f \( -name "*.scss" -o -name "*.css" -o -name "*.js" -o -name "*.html" -o -name "*.php" \))
-do
+# Initialize associative array for file extensions and line counts
+declare -A line_counts=( ["scss"]=0 ["css"]=0 ["js"]=0 ["html"]=0 ["php"]=0 ["py"]=0 ["sh"]=0 ["xml"]=0 ["xslt"]=0 )
+
+# Search for files and count lines
+while IFS= read -r -d '' file; do
+  extension="${file##*.}"
   lines=$(wc -l < "$file")
+  line_counts["$extension"]=$((line_counts["$extension"] + lines))
+done < <(find . -type f \( -name "*.scss" -o -name "*.css" -o -name "*.js" -o -name "*.html" -o -name "*.php" -o -name "*.py" -o -name "*.sh" -o -name "*.xml" -o -name "*.xslt" \) -print0)
 
-  case "$file" in
-    *.scss)
-      total_scss_lines=$((total_scss_lines + lines))
-      ;;
-    *.css)
-      total_css_lines=$((total_css_lines + lines))
-      ;;
-    *.js)
-      total_js_lines=$((total_js_lines + lines))
-      ;;
-    *.html)
-      total_html_lines=$((total_html_lines + lines))
-      ;;
-    *.php)
-      total_php_lines=$((total_php_lines + lines))
-      ;;
-  esac
+output_file="code_line_count.txt"
+cd data
+
+# Remove the output file if it exists
+[ -f "$output_file" ] && rm "$output_file"
+
+# Write line counts to the output file
+for ext in "${!line_counts[@]}"; do
+  count=${line_counts["$ext"]}
+  [ $count -ne 0 ] && echo "Total ${ext^^} lines: $count" >> "$output_file"
 done
 
-echo "Total SCSS lines: $total_scss_lines"
-echo "Total CSS lines: $total_css_lines"
-echo "Total JS lines: $total_js_lines"
-echo "Total HTML lines: $total_html_lines"
-echo "Total PHP lines: $total_php_lines"
+echo "Line count written to $output_file"
+sleep 5
