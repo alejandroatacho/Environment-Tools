119b0ae92dc3245eadee9c671fa8fcb0d11bb640
Hinamizawa <hinamizawa.github@gmail.com>
2023-04-05 14:24:58 -0400

    Update delete_sass.sh

--- tools/scripts/delete_sass.sh
+++ tools/scripts/delete_sass.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# Delete folder
+# Move to the parent directory
 cd ..
+
+# Greeting message
 echo "Hello Cutie _(:3」∠)_ , $(whoami), $(hostname)"
-echo "Deleting my SASS compiler mistakes!You don't deserve to get a salary from Bouke!"
-sleep 3
-rm -rf *.css.map wip.css mobile.css 2> /dev/null
+echo "Deleting my SASS compiler mistakes!"
+sleep 1
+
+# Find and delete all CSS and .map files except styles.css, user_custom_rules_main.css, and their respective .map files
+find . -type f \( -iname "*.css" -o -iname "*.map" \) ! \( -iname "styles.css" -o -iname "user_custom_rules_main.css" -o -iname "user_custom_rules_main.css.map" -o -iname "styles.css.map" \) -exec rm -f {} +
+
 if [ $? -eq 0 ]; then
   echo "Done!, all your compiler mistakes have been deleted."
-  sleep 2
+  sleep 1
   echo "Closing Terminal!"
   sleep 1
 else
   echo "Failed to delete files or missing files exist."
   sleep 1
 fi
-# open menu.html
+
+# Exit script
 exit
